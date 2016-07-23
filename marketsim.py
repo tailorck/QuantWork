abca564453d5d1e9f273adb2f365f3123c20c1eb
tailorck <cktailor34@gmail.com>
2016-07-23 17:05:19 -0400

    reformatted marketsim test_run method. added strategy

--- marketsim.py
+++ marketsim.py
@@ -74,26 +74,9 @@ def compute_portvals(start_date, end_date, orders_file, start_val):
     return portvals
 
 
-def test_run():
+def test_run(start_date, end_date, orders_file, start_val):
     """Driver function."""
-    # Define input parameters
-    '''
-    start_date = '2011-01-05'
-    end_date = '2011-01-20'
-    orders_file = os.path.join("orders", "orders-short.csv")
-    start_val = 1000000
-
-    start_date = '2011-01-10'
-    end_date = '2011-12-20'
-    orders_file = os.path.join("orders", "orders.csv")
-    start_val = 1000000
-
-    '''
-    start_date = '2011-01-14'
-    end_date = '2011-12-14'
-    orders_file = os.path.join("orders", "orders2.csv")
-    start_val = 1000000
-
+  
     # Process orders
     portvals = compute_portvals(start_date, end_date, orders_file, start_val)
 
@@ -104,31 +87,53 @@ def test_run():
     cum_ret, avg_daily_ret, std_daily_ret, sharpe_ratio = get_portfolio_stats(portvals)
 
     # Simulate a $SPX-only reference portfolio to get stats
-    prices_SPX = get_data(['$SPX'], pd.date_range(start_date, end_date))
-    prices_SPX = prices_SPX[['$SPX']]  # remove SPY
-    portvals_SPX = get_portfolio_value(prices_SPX, [1.0])
-    cum_ret_SPX, avg_daily_ret_SPX, std_daily_ret_SPX, sharpe_ratio_SPX = get_portfolio_stats(portvals_SPX)
+    prices_SPY = get_data(['SPY'], pd.date_range(start_date, end_date))
+    prices_SPY = prices_SPY[['SPY']]  # remove SPY
+    portvals_SPY = get_portfolio_value(prices_SPY, [1.0])
+    cum_ret_SPY, avg_daily_ret_SPY, std_daily_ret_SPY, sharpe_ratio_SPY = get_portfolio_stats(portvals_SPY)
 
-    # Compare portfolio against $SPX
+    # Compare portfolio against $SPY
     print "Data Range: {} to {}".format(start_date, end_date)
     print
     print "Sharpe Ratio of Fund: {}".format(sharpe_ratio)
-    print "Sharpe Ratio of $SPX: {}".format(sharpe_ratio_SPX)
+    print "Sharpe Ratio of $SPY: {}".format(sharpe_ratio_SPY)
     print
     print "Cumulative Return of Fund: {}".format(cum_ret)
-    print "Cumulative Return of $SPX: {}".format(cum_ret_SPX)
+    print "Cumulative Return of $SPY: {}".format(cum_ret_SPY)
     print
     print "Standard Deviation of Fund: {}".format(std_daily_ret)
-    print "Standard Deviation of $SPX: {}".format(std_daily_ret_SPX)
+    print "Standard Deviation of $SPY: {}".format(std_daily_ret_SPY)
     print
     print "Average Daily Return of Fund: {}".format(avg_daily_ret)
-    print "Average Daily Return of $SPX: {}".format(avg_daily_ret_SPX)
+    print "Average Daily Return of $SPY: {}".format(avg_daily_ret_SPY)
     print
     print "Final Portfolio Value: {}".format(portvals[-1])
 
     # Plot computed daily portfolio value
-    df_temp = pd.concat([portvals, prices_SPX['$SPX']], keys=['Portfolio', '$SPX'], axis=1)
-    plot_normalized_data(df_temp, title="Daily portfolio value and $SPX")
+    df_temp = pd.concat([portvals, prices_SPY['SPY']], keys=['Portfolio', 'SPY'], axis=1)
+    plot_normalized_data(df_temp, title="Daily portfolio value and SPY")
 
 if __name__ == "__main__":
-    test_run()
+  # Define input parameters
+  '''
+  start_date = '2011-01-05'
+  end_date = '2011-01-20'
+  orders_file = os.path.join("orders", "orders-short.csv")
+  start_val = 1000000
+
+  start_date = '2011-01-10'
+  end_date = '2011-12-20'
+  orders_file = os.path.join("orders", "orders.csv")
+  start_val = 1000000
+
+  start_date = '2011-01-14'
+  end_date = '2011-12-14'
+  orders_file = os.path.join("orders", "orders2.csv")
+  start_val = 1000000
+  '''
+  start_date = '2007-12-31'
+  end_date = '2009-12-31'
+  orders_file = 'out.txt'
+  start_val = 10000
+
+  test_run(start_date, end_date, orders_file, start_val)
